c99b1a3f52d82251b4b7e7c5645394f9cdadb843
Ronny139 <94130958+Ronny139@users.noreply.github.com>
2022-07-05 19:51:21 +0530

    Myfarmo
    Farmer helping website

---
+++ js/crop.js
@@ -0,0 +1,157 @@
+var sb = document.getElementById("sb");
+var aa = "images/client.png";
+var a = "images/client.png";
+var b = "images/logo.png";
+var c = "images/contact-bg.jpg";
+var arr0 = [" <br><br><br>Acidic Nature: <br>So, soil which has Ph from 0 To 3 is strongly acidic soil which is not good for the growth plants and crops. In this type of soil we have to increase their pH  upto 4 so that we can take some crops in this type of soil. Few techniques to reduce it are as follows:", "Technique 1: Adding Limestone <br>Limestones are very good in raising the pH of the soil. If the soil is very acidic (pH is very low) you can reduce the acidity of the soil by adding limestones into the soil .If you want to quickly reduce the acidity use the hydrated lime, just remember it is hard to control though.", "Technique 2 :Adding wood ashes <br>Hardwood ash contains calcium. So adding wood ash will raise the pH level of the soil. But it is lighter compared to lime as it has half the calcium that the lime has. So if you want to add wood ash in place of lime apply twice as much lime.", "Technique 3: Add baking soda <br> Baking soda is a cost-effective method to increase soil pH . It also works very quickly and can produce results in a few days.After using this methods the soil of the pH is increase  about 4 to 5 so know we can take any crops in which are taken in 4 to 5 pH of soil.<vr><br> <b>best ph range for crops from 4 to 9</b>"];
+var arr00 = ["img/4.jfif", "img/1.png", "img/2.jpg", "img/3.jpg"];
+var arr8=["img/23.jfif","img/24.png","img/25.jpg"];
+var arr9=["<br><br><br> <br><br><br> So , the soil with pH 9 to 14 is strongly basic in nature .the basic soil are not good for the growth of the soil<br>How Do You Decrease Soil pH?","<br><br> Technique 1: <br>Ammonium forms nitric acid as it oxidizes hence adding more acid to the soil leading to a decrease in soil pH.","<br><br><br> Technique 2: Adding sulphur<br>Elemental sulfur is also one of the best compounds to decrease soil ph. Apply an amount of 300 to 500 per hectare to your alkaline soils. Elemental sulfur oxidizes slowly forming sulphuric acid."];
+//var arr = ["libero esse voluptatem! Placeat earum delectus beatae ipsam voluptatibus est modi adipisci! Eveniet assumenda et vel officia fugit expedita earum aliquid natus nesciunt rem nam amet a, vero odio inventore nobis necessitatibus, ducimus nostrum quo at nisi sed? Aliquam a vel nostrum nulla et sunt rem iure totam veniam ex, debitis at ea? Quo libero aut, repellendus laborum fugiat omnis illo dolorum reiciendis, unde similique sunt obcaecati inventore accusamus, soluta quod repudiandae iusto est. Odit ea culpa nobis facere. Voluptatem, consequatur! Neque, unde officia?", "it is suitable for ph 1 is suitable for ph 0 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, magni ab. Beatae ad architecto in  ", "it is suitable for ph 2"];
+var arr = ["aa", "bb", "cc"];
+var arr31 = ["img/5.jfif", "img/6.jfif", "img/7.jfif", "img/8.jfif"];
+var arr32 = ["<br><br><br>Potatoes are a cool-weather crop, but the leafy tops can’t tolerate more than a light frost. Plant potatoes so they come to harvest when the weather is cool but not cold. In cold-winter climates, plant seed potatoes in the garden in spring as soon as the soil can be worked—usually about 3 weeks before the planting.", "<br><br><br>The blueberry is easy to plant and grow. The blueberry plant is best planted and grown where summers are cool and the soil is acidic. There are varieties that will grow in warm, dry summer regions.Blueberries are long-lived. Plant them where they can grow undisturbed for 10 or more years. Blueberries require the soil to have a pH between 4.3 to 5.5 pH. This is unique, because most fruit and vegetable plants do best in a relatively neutral pH soil between about 6.5-7.5.", "<br><br><br>This trumpet-shaped flower has a violet-blue color that looks stunning in patios and lawns. It grows fast and appreciates part sun to full sunlight for best growth. The starry flower in a blue hue continues to display from late springs to early summers. Once the flowers fade, the foliage turns yellowish gold, giving the plant an exotic appearance.For true blue flowers, the hydrangeas need to be grown in acidic soil with a pH of 5.5 or lower.", "<br><br><br>Pines are softwoods, but commercially they may be designated as soft pines or hard pines. Soft pines, such as white, sugar, and piñon pines, have relatively soft timber, needles in bundles of five (less commonly, one to four), stalked  with scales lacking prickles, and little resin. Their wood is close-grained, with thin, nearly white sapwood; the sheaths of the leaf clusters are deciduous, and the leaves contain a single fibrovascular bundle."];
+// var arr41=[];
+// var arr42=[];
+var arr51 = ["img/9.jfif", "img/10.jfif", "img/11.jfif", "img/12.jfif", "img/13.jfif"];
+var arr52 = ["<br><br><br>o	Plant peanuts in full sun.<br>o	Peanuts grow best in loose, well-drained soil rich in organic matter. A sandy-loamy soil is best. The soil must be loose so that the pegs can penetrate and grow.<br>o	Double-dig clay soil and add gypsum and aged compost.<br>o	Peanuts prefer a soil pH of 5.8 to 6.2.", "<br><br><br>Sweet potatoes grow best in long-season regions (150 to 160 days of frost-free weather) where summers are hot but not excessively rainy. The optimal temperature range for best growth is 70°to 85°F (21°-29°C).<br>Sweet Potato planting details<br>o	Planting depth: Set slips 4 to 8 inches deepo	Space between plants: 15 incheso	Days to harvest: 100 to 170o	Storage period: 4-6 monthso	Suggested varieties: Allgood, Beauregard, Centennial, Jewel, Vardamen<br>", "<br>br>br>br>Carrots are among the easiest garden vegetables to grow—and the most bountiful–given the right conditions.br>Grow carrots in the cool time of the year—spring and autumn. <br>Plant carrots in loose, compost-rich soil free of pebbles, stones, or other obstructions.", "<br>Grow corn in the warm time of the year. Direct sow corn after all danger of frost has passed. <br>Make succession sowings for a continuous harvest throughout the growing season. Three types of sweet corn Corn growing starts with deciding which type of corn to plant: standard, sugar-enhanced, or super-sweet.", "<br><br><br>Garlic can be planted in spring or in fall. <br>Spring planted garlic is harvested late in the summer or early autumn. <br>Fall planted garlic is harvested in mid-summer, the summer after fall planting. Fall planted garlic will produce larger bulbs and cloves than spring-planted garlic.", ""];
+var arr61 = ["img/14.jfif", "img/15.jfif", "img/16.jfif", "img/17.jfif", "img/18.jfif"];
+var arr62 = ["<br><br><br><br>Beets are rich in folate (vitamin B9) which helps cells grow and function. Folate plays a key role in controlling damage to blood vessels, which can reduce the risk of heart disease and stroke. Beets are naturally high in nitrates, which are turned into nitric oxide in the body.", "<br><br><br><br>Onions contain compounds called diallyl disulfide and lipid transfer protein, which can cause allergy symptoms like asthma, runny nose, nasal congestion, red eyes, itchy eyes and nose, and contact dermatitis, characterized by a red, itchy rash ( 9 , 10 ).", "<br><br><br>A root vegetable with a crisp texture and a peppery hot flavour. The hotness of the radish varies from mild to very strong, depending on the age and type. The different types  come in a variety of sizes, shapes, and colours.", "<br><br><br>               Watermelon is grown in favourable climates from tropical to temperate regions worldwide for its large edible fruit, which is a berry with a hard rind and no internal divisions, and is botanically called a pepo.", "<br><br><br>            Mustard, condiment made from the pungent seeds of either of two main herbs belonging to the family Brassicaceae. The principal types are white, or yellow, mustard (Sinapis alba), a plant of Mediterranean origin; and brown, or Indian, mustard (Brassica juncea), which is of Himalayan origin."];
+ var arr71=["img/18.jfif","img/19.jfif","img/20.jfif","img/21.jfif","img/22.jfif"];
+ var arr72=["<br><br><br>                   Asparagus, or garden asparagus, folk name sparrow grass, scientific name Asparagus officinalis, is a perennial flowering plant species in the genus Asparagus. Its young shoots are used as a spring vegetable. It was once classified in the lily family, like the related Allium species, onions and garlic.","<br><br><br>   <br><br><br>  <br><br><br>  <br><br><br>                     The common sunflower (H. annuus) is an annual herb with a rough hairy stem 1–4.5 metres (3–15 feet) high and broad, coarsely toothed, rough leaves 7.5–30 cm (3–12 inches) long arranged in spirals. The attractive heads of flowers are 7.5–15 cm wide in wild specimens and often 30 cm or more in cultivated types.","<br><br><br>                      Cabbage, comprising several cultivars of Brassica oleracea, is a leafy green, red (purple), or white (pale green) biennial plant grown as an annual vegetable crop for its dense-leaved heads. It is descended from the wild cabbage (B. oleracea var).","<br><br><br>They include such apparently diverse forms as cabbage, kale, brussels sprouts, kohlrabi, broccoli and cauliflower (Figure 1). All of these were, however, domesticated from one ancestral species, the wild cabbage, Brassica oleracea[1].","<br><br><br><br><br><br>  <br><br><br>  <br><br><br>  The pea is most commonly the small spherical seed or the seed-pod of the pod fruit Pisum sativum. Each pod contains several peas, which can be green or yellow. A pea, although treated as a vegetable in cooking, is botanically a fruit; the term is most commonly used to describe the small spherical seeds or the pods of the legume Pisum sativum.[1] This was the original model organism used by Gregor Mendel in his early work on genetics"];
+// var arr81[];
+// var arr82[];
+
+
+
+function fun() {
+
+
+    if (Number(sb.value) >= 0 && Number(sb.value) <= 3) {
+        document.getElementById("cropimg00").src = arr00[0];
+        document.getElementById("para00").innerHTML = arr0[0];
+        document.getElementById("cropimg11").src = arr00[1];
+        document.getElementById("para11").innerHTML = arr0[1];
+        document.getElementById("cropimg22").src = arr00[2];
+        document.getElementById("para22").innerHTML = arr0[2];
+        document.getElementById("cropimg33").src = arr00[3];
+        document.getElementById("para33").innerHTML = arr0[3];
+
+    } else if (Number(sb.value) >= 3.1 && Number(sb.value) <= 5) {
+        document.getElementById("name4").innerHTML = "Potato";
+        document.getElementById("cropimg4").src = arr31[0];
+        document.getElementById("para4").innerHTML = arr32[0];
+        document.getElementById("name5").innerHTML = "BlueBerry";
+        document.getElementById("cropimg5").src = arr31[1];
+        document.getElementById("para5").innerHTML = arr32[1];
+        document.getElementById("name6").innerHTML = "Blue Flowered";
+        document.getElementById("cropimg6").src = arr31[2];
+        document.getElementById("para6").innerHTML = arr32[2];
+        document.getElementById("name7").innerHTML = "pine";
+        document.getElementById("cropimg7").src = arr31[3];
+        document.getElementById("para7").innerHTML = arr32[3];
+        // new
+        document.getElementById("name15").innerHTML = "Radish";
+        document.getElementById("cropimg15").src = arr61[2];
+        document.getElementById("para15").innerHTML = arr62[2];
+        document.getElementById("name16").innerHTML = "Watermelon";
+        document.getElementById("cropimg16").src = arr61[3];
+        document.getElementById("para16").innerHTML = arr62[3];
+        document.getElementById("name17").innerHTML = "Mustard";
+        document.getElementById("cropimg17").src = arr61[4];
+        document.getElementById("para17").innerHTML = arr62[4];
+
+    } else if (Number(sb.value) >= 5.1 && Number(sb.value) <= 6) {
+        document.getElementById("name8").innerHTML = "Peanut";
+        document.getElementById("cropimg8").src = arr51[0];
+        document.getElementById("para8").innerHTML = arr52[0];
+        document.getElementById("name9").innerHTML = "Sweet Potato";
+        document.getElementById("cropimg9").src = arr51[1];
+        document.getElementById("para9").innerHTML = arr52[1];
+        // new
+        document.getElementById("name15").innerHTML = "Radish";
+        document.getElementById("cropimg15").src = arr61[2];
+        document.getElementById("para15").innerHTML = arr62[2];
+        document.getElementById("name16").innerHTML = "Watermelon";
+        document.getElementById("cropimg16").src = arr61[3];
+        document.getElementById("para16").innerHTML = arr62[3];
+        document.getElementById("name17").innerHTML = "Mustard";
+        document.getElementById("cropimg17").src = arr61[4];
+        document.getElementById("para17").innerHTML = arr62[4];
+        // new
+        document.getElementById("name10").innerHTML = "Carrots";
+        document.getElementById("cropimg10").src = arr51[2];
+        document.getElementById("para10").innerHTML = arr52[2];
+        document.getElementById("name11").innerHTML = "Corn";
+        document.getElementById("cropimg11").src = arr51[3];
+        document.getElementById("para11").innerHTML = arr52[3];
+        document.getElementById("name12").innerHTML = "Garlic";
+        document.getElementById("cropimg12").src = arr51[4];
+        document.getElementById("para12").innerHTML = arr52[4];
+
+
+    } else if (Number(sb.value) >= 6.1 && Number(sb.value) <= 7) {
+        document.getElementById("name13").innerHTML = "Beet";
+        document.getElementById("cropimg13").src = arr61[0];
+        document.getElementById("para13").innerHTML = arr62[0];
+        document.getElementById("name14").innerHTML = "Onion";
+        document.getElementById("cropimg14").src = arr61[1];
+        document.getElementById("para14").innerHTML = arr62[1];
+        document.getElementById("name15").innerHTML = "Radish";
+        document.getElementById("cropimg15").src = arr61[2];
+        document.getElementById("para15").innerHTML = arr62[2];
+        document.getElementById("name16").innerHTML = "Watermelon";
+        document.getElementById("cropimg16").src = arr61[3];
+        document.getElementById("para16").innerHTML = arr62[3];
+        document.getElementById("name17").innerHTML = "Mustard";
+        document.getElementById("cropimg17").src = arr61[4];
+        document.getElementById("para17").innerHTML = arr62[4];
+
+    } else if (Number(sb.value) >= 7.1 && Number(sb.value) <= 8) {
+        document.getElementById("name18").innerHTML = "Asparagus";
+        document.getElementById("cropimg18").src = arr71[0];
+        document.getElementById("para18").innerHTML = arr72[0];
+        document.getElementById("name19").innerHTML = "Sunflower";
+        document.getElementById("cropimg19").src = arr71[1];
+        document.getElementById("para19").innerHTML = arr72[1];
+        document.getElementById("name20").innerHTML = "Cabbage";
+        document.getElementById("cropimg20").src = arr71[2];
+        document.getElementById("para20").innerHTML = arr72[2];
+        document.getElementById("name21").innerHTML = "Couliflower";
+        document.getElementById("cropimg21").src = arr71[3];
+        document.getElementById("para21").innerHTML = arr72[3];
+        document.getElementById("name22").innerHTML = "Pea";
+        document.getElementById("cropimg22").src = arr71[4];
+        document.getElementById("para22").innerHTML = arr72[4];
+
+    } else if (Number(sb.value) >= 8.1 && Number(sb.value) <= 9) {
+        document.getElementById("name13").innerHTML = "Beet";
+        document.getElementById("cropimg13").src = arr61[0];
+        document.getElementById("para13").innerHTML = arr62[0];
+        document.getElementById("name14").innerHTML = "Onion";
+        document.getElementById("cropimg14").src = arr61[1];
+        document.getElementById("para14").innerHTML = arr62[1];
+        document.getElementById("name15").innerHTML = "Radish";
+        document.getElementById("cropimg15").src = arr61[2];
+        document.getElementById("para15").innerHTML = arr62[2];
+        document.getElementById("name16").innerHTML = "Watermelon";
+        document.getElementById("cropimg16").src = arr61[3];
+        document.getElementById("para16").innerHTML = arr62[3];
+        document.getElementById("name17").innerHTML = "Mustard";
+        document.getElementById("cropimg17").src = arr61[4];
+        document.getElementById("para17").innerHTML = arr62[4];
+
+    } else if (Number(sb.value) >= 9.1 && Number(sb.value) <= 14) {
+        document.getElementById("cropimg88").src = arr8[0];
+        document.getElementById("para88").innerHTML = arr9[0];
+        document.getElementById("cropimg99").src = arr8[1];
+        document.getElementById("para99").innerHTML = arr9[1];
+        document.getElementById("cropimg100").src = arr8[2];
+        document.getElementById("para100").innerHTML = arr9[2];
+       
+    } else {
+        document.getElementById("name").innerHTML = "enter valid ph value";
+
+    }
+
+}
